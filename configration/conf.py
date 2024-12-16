736f9278a4e5e06c7e973e58b3c61695bd8913b7
Aboobacker Bilal P <aboobackerbilalp@gmail.com>
2024-12-16 22:52:21 +0530

    created a fastapi project

---
+++ configration/conf.py
@@ -0,0 +1,19 @@
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    MONGO_URL: str
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
+
+client = MongoClient(settings.MONGO_URL, server_api=ServerApi('1'))
+
+db = client.todo_db
+collection = db["todo_data"]
+rec_collection = db["records"]
